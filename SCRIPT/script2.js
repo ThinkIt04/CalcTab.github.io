be19f16b35c43b9465421349d537e0809d62d2ef
ThinkIt04 <72980564+ThinkIt04@users.noreply.github.com>
2020-11-30 19:18:10 +0530

    ...

---
+++ SCRIPT/script2.js
@@ -0,0 +1,8 @@
+function menu(){
+     document.getElementById('form2').style.height = "0";
+     document.getElementById('form1').style.height = "auto";
+}
+function m1(){
+     document.getElementById('form1').style.height = "0";
+     document.getElementById('form2').style.height = "auto";
+}
