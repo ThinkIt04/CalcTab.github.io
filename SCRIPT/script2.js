01cb91fe5072fa933fd0dc9f61e48c8859aadff7
ThinkIt04 <72980564+ThinkIt04@users.noreply.github.com>
2020-12-02 09:17:31 +0530

    Revert "Revert "....""
    This reverts commit 062e4d7ac4a1024f3121622c0236502c01529236.

--- SCRIPT/script2.js
+++ SCRIPT/script2.js
@@ -1,17 +1,26 @@
 function menu(){
      document.getElementById('form2').style.height = "0";
      document.getElementById('form3').style.height = "0";
+     document.getElementById('form4').style.height = "0";
      document.getElementById('form1').style.height = "auto";
 }
 function m1(){
      document.getElementById('form1').style.height = "0";
      document.getElementById('form3').style.height = "0";
+     document.getElementById('form4').style.height = "0";
      document.getElementById('form2').style.height = "auto";
 }
 function m2(){
      document.getElementById('form1').style.height = "0";
      document.getElementById('form2').style.height = "0";
+     document.getElementById('form4').style.height = "0";
      document.getElementById('form3').style.height = "auto";
+}
+function m3(){
+     document.getElementById('form1').style.height = "0";
+     document.getElementById('form2').style.height = "0";
+     document.getElementById('form3').style.height = "0";
+     document.getElementById('form4').style.height = "auto";
 }
 function copyInput2() {
      var copy = document.getElementById('display');
